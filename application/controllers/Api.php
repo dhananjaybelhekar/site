4bdea3148562e196d2c945acbbf95c28c3bb079a
dhananjay431 <dbel431@gmail.com>
2017-11-17 11:31:35 +0530

    add api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -18,4 +18,87 @@ class Api extends CI_Controller {
 	{
 		echo json_encode(array('foo'=>456465,'bar'=>8879878));
 	}
+		public function logout()
+	{
+		$this->load->database();
+		$req = json_decode($this->input->raw_input_stream);	
+		if(isset($req->result))
+		{
+			$qr =$this->db->delete('log', array('fingerprint' =>$req->result));
+			// $this->db->query($qr);
+			$arr= array();
+			if($qr){
+				$arr['url']= 'login';		
+				$arr['success']= true;
+			}
+			echo json_encode($arr);
+		}
+	}
+	public function login()
+	{
+		/*
+		$startDate = date('m-d-Y H:i:s', time());
+		$endDate = date('m-d-Y H:i:s', time() + (60 * 30));
+		echo $startDate . "\n";
+		echo $endDate;
+		*/
+		$this->load->database();
+		$this->load->library('user_agent');
+		$req = json_decode($this->input->raw_input_stream);	
+		$arr = array();
+		if(isset($req->result) && isset($req->user) && isset($req->pass))
+		$user = $this->db->query("SELECT username FROM `user` WHERE `username` = '".$req->user."' && `pass` = '".md5($req->pass)."'")->result_array();
+		if(count($user) > 0){
+			$logArr =array(
+				'user'=>$user[0]['username'],
+				'agent'=>$this->agent->agent_string(),
+				'endDate'=>date('Y-m-d H:i:s', time() + (60 * 60 * 60)),
+				'fingerprint'=>$req->result,
+				'ip'=>$this->input->ip_address()
+			);
+		$this->db->insert('log', $logArr);
+		$arr['user'] = $user[0];	
+		$arr['token']=$this->generateRandomString();
+		$arr['url']= '/main';
+		//$arr['log']=  $this->fingerprintcheck($req->result);
+		$arr['success']=true;
+	}
+	else{
+		$arr['success']=false;
+	}
+		
+		
+		echo json_encode($arr);
+	}
+	public function fingerprintcheck()
+	{
+		$req = json_decode($this->input->raw_input_stream);	
+		$arr = array();
+		$this->load->database();
+		$log =  $this->db->query("SELECT * FROM `log` WHERE `fingerprint` = '".$req->id."' AND `endDate` >  '".date('Y-m-d H:i:s', time())."'")
+		->result_array();
+		if(count($log) != 0)
+		{
+			$arr['success']=true;
+			$arr['url']='/main';
+		}else
+		{
+			$arr['success']=false;
+			$arr['url']='/login';
+			$arr['qr']= "SELECT * FROM `log` WHERE `fingerprint` = '".$req->id."' AND `endDate` >  '".date('Y-m-d H:i:s', time())."'";
+		}
+		echo json_encode($arr);	
+		//echo "SELECT * FROM `log` WHERE `fingerprint` = '".$req->id."' AND `endDate` >  '".date('Y-m-d H:i:s', time())."'";
+	}
+	//
+	public function generateRandomString($length = 64) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0
+     ,$charactersLength - 1)];
+    }
+    return $randomString;
+	}
 }
